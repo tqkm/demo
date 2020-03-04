968b31ccf84c6ae8cd34b8dd88ed1e8e7a9cad71
da wang <563194326@qq.com>
2020-03-04 22:06:40 +0800

    测试

---
+++ src/main/java/com/example/demo/thread/Local.java
@@ -0,0 +1,25 @@
+package com.example.demo.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Local {
+
+    public static void main(String[] args) {
+        ExecutorService pool = Executors.newCachedThreadPool();
+        //ThreadLocal<String> threadLocal = new ThreadLocal<>();
+        InheritableThreadLocal<String> threadLocal = new InheritableThreadLocal<>();
+        threadLocal.set("123");
+        for (int i = 0; i < 3; i ++)
+            pool.execute(() -> {
+                System.out.println(threadLocal.get());
+                threadLocal.set(Thread.currentThread().getName());
+                System.out.println(threadLocal.get());
+            });
+        System.out.println(threadLocal.get());
+        pool.shutdown();
+        var ss = 1234;
+        System.out.println(ss);
+    }
+
+}
---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+        String app = "123";
+        System.out.println(123);
+    }
+
+}
