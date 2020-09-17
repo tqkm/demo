94f4c27c513904e49b4ae18f22a4ee9cd550e33c
da wang <563194326@qq.com>
2020-09-17 22:28:31 +0800

    Revert "first"
    This reverts commit c7fe3db7

--- src/main/java/com/example/demo/thread/Local.java
+++ src/main/java/com/example/demo/thread/Local.java
@@ -32,8 +32,6 @@ public class Local {
         p3.setIssSt(1);
         System.out.println(m.put(p2, "3"));
         System.out.println(m);
-        //第一次提交
-        //第二次提交
     }
 
     private static void a() throws FileNotFoundException {
